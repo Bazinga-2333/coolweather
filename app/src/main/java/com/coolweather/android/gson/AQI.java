32d395412592104eebd40864fdb2e29fb9e71b36
Bazinga2333 <1399591397@qq.com>
2017-08-30 19:57:54 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by 小怪兽的窝 on 2017/8/29.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
